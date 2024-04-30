1ba5152f5d85f60792a9c8230f02ea2ade33e624
Junichi Kako <147279646+JunichiKako@users.noreply.github.com>
2024-04-30 12:56:48 +0900

    fix:google map

--- app/components/pokemon-park.tsx
+++ app/components/pokemon-park.tsx
@@ -5,11 +5,12 @@ import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 const container = {
     width: "100%",
     height: "460px",
+
 };
 
 const position = {
-    lat: 35.182253007459444,
-    lng: 136.90534328438358,
+    lat: 34.24731704584732,
+    lng: 133.93299939792632,
 };
 
 const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!;
@@ -20,16 +21,17 @@ if (!googleMapsApiKey) {
 
 export default function PokemonPark() {
     return (
-        <div className="flex-1 overflow-y-auto hidden-scrollbar bg-red-500">
+        <div className="flex-1 overflow-y-auto hidden-scrollbar">
             <h2>Poke Park Map</h2>
-            <div className="w-full">
+            <div className="w-full rounded-md">
                 <LoadScript googleMapsApiKey={googleMapsApiKey}>
                     <GoogleMap
                         mapContainerStyle={container}
                         center={position}
                         zoom={15}
+
                     >
-                        <MarkerF position={position} label={"名古屋城"} />
+                        <MarkerF position={position} label={"ヤドン公園"} />
                     </GoogleMap>
                 </LoadScript>
             </div>
