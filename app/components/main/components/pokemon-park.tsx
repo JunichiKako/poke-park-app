4b49bf5c26a7a90dac1b4f9bb1e324dad869d2df
Junichi Kako <147279646+JunichiKako@users.noreply.github.com>
2024-05-16 15:34:17 +0900

    add: map自体のzoom 機能

--- app/components/main/components/pokemon-park.tsx
+++ app/components/main/components/pokemon-park.tsx
@@ -1,12 +1,30 @@
 "use client";
 
 import { Pokemon } from "@/types/pokemon";
-import { APIProvider, AdvancedMarker, Map, Pin } from "@vis.gl/react-google-maps";
+import {
+  APIProvider,
+  AdvancedMarker,
+  ControlPosition,
+  Map,
+  MapControl,
+  Pin,
+} from "@vis.gl/react-google-maps";
 import Image from "next/image";
 import { useMapData } from "../map-provider";
+import { useMemo, useState } from "react";
+import { CustomZoomControl } from "./custom-zoom-control";
+import "@googlemaps/extended-component-library/overlay_layout.js";
 
 export default function PokemonPark({ pokemons }: { pokemons: Pokemon[] }) {
   const { selectedPokemon } = useMapData();
+
+  const [controlPosition, setControlControlPosition] = useState<ControlPosition>(
+    ControlPosition.LEFT_BOTTOM
+  );
+
+  const [zoom, setZoom] = useState(4);
+
+  const center = useMemo(() => ({ lat: 35.69025034295892, lng: 139.69001842593207 }), []);
 
   const getCenter = () => {
     if (Array.isArray(selectedPokemon.location)) {
@@ -21,16 +39,14 @@ export default function PokemonPark({ pokemons }: { pokemons: Pokemon[] }) {
       <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
         <Map
           className="size-full *:!bg-transparent [&_iframe+div]:!border-ring [&_iframe+div]:rounded-lg"
-          defaultCenter={{
-            lat: 35.69025034295892,
-            lng: 139.69001842593207,
-          }}
+          defaultCenter={center}
           defaultZoom={5}
           gestureHandling={"greedy"}
           disableDefaultUI
           mapId={"f7865720b0868f38"}
           center={getCenter() as { lat: number; lng: number } | null}
-          zoom={selectedPokemon.location ? 8 : 5}
+          onZoomChanged={(ev) => setZoom(ev.detail.zoom)}
+          zoom={selectedPokemon.location ? 8 : zoom}
         >
           {pokemons.map((pokemon) => {
             if (Array.isArray(pokemon.location)) {
@@ -67,6 +83,23 @@ export default function PokemonPark({ pokemons }: { pokemons: Pokemon[] }) {
               return null;
             }
           })}
+
+          <MapControl position={ControlPosition.TOP_LEFT}>
+            <div
+              style={{
+                background: "white",
+                padding: "1em",
+              }}
+            >
+              Zoom: {zoom.toFixed(2)}
+            </div>
+          </MapControl>
+
+          <CustomZoomControl
+            controlPosition={controlPosition}
+            zoom={zoom}
+            onZoomChange={(zoom) => setZoom(zoom)}
+          />
         </Map>
       </APIProvider>
     </div>
